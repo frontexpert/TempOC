d9110d657f15768b51956fb34eab4c20f75646b9
frontexpert <frontexport1987@gmail.com>
2018-03-27 16:24:24 +0800

    finished login page ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,23 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { CorePageModule } from '../pages/core/core.module';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LoginPage,
   ],
   imports: [
     BrowserModule,
+    CorePageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage
   ],
   providers: [
     StatusBar,
