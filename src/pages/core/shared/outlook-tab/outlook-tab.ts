e61648c041bf8e86f36a1aaaee47470a1cc57e17
frontexpert <frontexport1987@gmail.com>
2018-04-01 12:04:06 +0800

    added expandable component and update outlook-tab page

--- src/pages/core/shared/outlook-tab/outlook-tab.ts
+++ src/pages/core/shared/outlook-tab/outlook-tab.ts
@@ -5,7 +5,33 @@ import { Component, EventEmitter, Output, Input } from '@angular/core';
   templateUrl: 'outlook-tab.html'
 })
 export class OutlookTabComponet {
+
+  items: any = [];
+  itemExpandHeight: number = 100;
+
   constructor() {
-    
+    this.items = [
+      {expanded: false},
+      {expanded: false},
+      {expanded: false},
+      {expanded: false},
+      {expanded: false}
+    ];
+  }
+
+  expandItem(item){
+ 
+    this.items.map((listItem) => {
+
+        if(item == listItem){
+            listItem.expanded = !listItem.expanded;
+        } else {
+            listItem.expanded = false;
+        }
+
+        return listItem;
+
+    });
+
   }
 }
