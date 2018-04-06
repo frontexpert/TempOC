2f0a81559454e763388ce6caadd1662126a9b049
mobiledev <artem.nimenko@yandex.com>
2018-04-06 01:28:31 -0700

    Implement of Pick, Select, remove of photos on POTO tap page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
+import { ImagePicker } from '@ionic-native/image-picker';
 
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
@@ -35,6 +37,8 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    ImagePicker,
     Globals,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     Api,
--- src/pages/core/shared/photo-tab/photo-tab.ts
+++ src/pages/core/shared/photo-tab/photo-tab.ts
@@ -1,11 +1,71 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
+import { Camera } from '@ionic-native/camera';
+import { ImagePicker } from '@ionic-native/image-picker';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'photo-tab',
   templateUrl: 'photo-tab.html'
 })
 export class PhotoTabComponet {
-  constructor() {
-    
+  photos = [];
+
+  constructor(private imagePicker: ImagePicker, private camera: Camera, private sanitizer: DomSanitizer) {
+  }
+
+  takeNewPhoto() {
+    let cameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,      
+      quality: 80,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
+    }
+  
+    this.camera.getPicture(cameraOptions).then(imageData => {
+        let image_url = "data:image/jpeg;base64, " + imageData;
+        let  camera_image_url = this.sanitizer.bypassSecurityTrustUrl(image_url);
+        let photo = {
+          url: camera_image_url,
+          checked: false
+        };
+        this.photos.push(photo);
+      }).catch(err => console.log(err));
+  }
+  
+  selectNewPhoto() {
+    let options = {
+      maximumImagesCount: 15,
+      width: 1000,
+      height: 1000,
+      quality: 80,
+      outputType: 1
+    }
+    this.imagePicker.getPictures(options).then(imagesData => {
+      for (let i = 0; i < imagesData.length; i++) {
+        let image_url = "data:image/jpeg;base64, " + imagesData[i];
+        let  picker_image_url = this.sanitizer.bypassSecurityTrustUrl(image_url);
+        console.log(picker_image_url);
+        let photo = {
+          url: picker_image_url,
+          checked: false
+        };
+        this.photos.push(photo);
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  deletePhoto() {
+    for (let i = this.photos.length - 1; i >= 0; i--) {
+      if (this.photos[i].checked == true) {
+        this.photos.splice(i, 1);
+      }
+    }
+  }
+
+  toggle(index) {
+    this.photos[index].checked = !this.photos[index].checked;
   }
 }
