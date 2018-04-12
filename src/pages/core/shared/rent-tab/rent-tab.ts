f3810c84e24388159032490d24d1a127ba6e444f
frontexpert <frontexport1987@gmail.com>
2018-04-12 15:54:35 +0800

    finshed noleggi ui

--- src/pages/core/shared/rent-tab/rent-tab.ts
+++ src/pages/core/shared/rent-tab/rent-tab.ts
@@ -5,7 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: 'rent-tab.html'
 })
 export class RentTabComponet {
+  // pie
+  colorScheme = {
+    domain: ['#ff8ba4', '#86c7f3', '#ffe29a', '#AAAAAA']
+  };
+  showLabels = false;
+  explodeSlices = false;
+  doughnut = false;
+  view: any[] = [120, 120];
+
+  single = [
+    {
+      "name": "Germany",
+      "value": 8940000
+    },
+    {
+      "name": "USA",
+      "value": 5000000
+    },
+    {
+      "name": "France",
+      "value": 7200000
+    }
+  ];
+
   constructor() {
     
+  }
+
+  onSelect(event) {
+    console.log(event);
   }
 }
