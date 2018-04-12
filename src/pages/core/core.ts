09e26949f6538e41b5e4427becfc611a09d31922
mobiledev <artem.nimenko@yandex.com>
2018-04-12 08:42:01 -0700

    Enhance of In-App-Browser.

--- src/pages/core/core.ts
+++ src/pages/core/core.ts
@@ -36,21 +36,72 @@ export class CorePage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad CorePage');
   }
-
   onSidebarNavigate(item) {
     if (item == "car") {
-      let options: InAppBrowserOptions = {
-        footer: "no",
-        toolbarposition: "top"
-      }
-      const browser = this.iab.create('http://oxygen2.ilcarrozziere.it', '_blank', options);
-      browser.show();
+      // let options: InAppBrowserOptions = {
+      //   footer: "no",
+      //   toolbarposition: "top"
+      // }
+      //const browser = this.iab.create('http://oxygen2.ilcarrozziere.it', '_blank', options);
+      //browser.show();
+      this.openInAppBrowser();
     }
     else {
       this.nav.push(this.pages[item]);
     }
   }
 
+  openInAppBrowser() {
+    var close;
+    var closeLoop;
+    let options: InAppBrowserOptions = {
+      footer: "no",
+      location: "no",
+      toolbar: "no"
+    }
+    
+    const win = this.iab.create('http://www.sitesolutions.it', '_blank', options);
+ 
+    win.on('loaderror').subscribe(event => {
+      console.log('load error');
+      alert('load error');
+      console.log (event);
+    }); 
+    win.on('loadstop').subscribe(event => {
+        //injecting the CSS
+        win.insertCSS({
+          code: ".youtube_done_button { position: fixed; bottom: 0; width: 100%; background: rgba(0, 0, 0, 0.8); color: #2196F3; padding: 10px; font-size: 20px;}"   
+        });
+  
+ 
+        //setting close to false when the InAppBrowser is opened
+        win.executeScript({
+          code: "localStorage.setItem('close', 'false');"
+        });
+
+        //creating and attaching a button with click listener to the opened page
+        win.executeScript({
+          code: "(function() { var body = document.querySelector('body'); var button = document.createElement('div'); button.innerHTML = 'Done'; button.classList.add('youtube_done_button'); button.onclick = function() { localStorage.setItem('close', 'true');  }; body.appendChild(button); })();"                
+        });
+ 
+        closeLoop = setInterval(function() {
+          win.executeScript({
+
+            code: "localStorage.getItem('close');"
+
+          }).then( function(values) {
+              close = values[0];
+              console.log("close=" + close);
+              if (close == "true") {
+                  clearInterval(closeLoop);
+                  win.close();
+              }
+          });
+        }, 1000);
+    });
+    win.show();
+  }
+  
   /**
    * Log out the app
    */
