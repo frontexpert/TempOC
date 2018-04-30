57a407e841919bb38f6f2fe7f86e84199ff48874
mobiledev <artem.nimenko@yandex.com>
2018-04-30 06:26:09 -0700

    Update of Protices list loading.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { CorePage } from '../core/core';
+import { PracticesProvider } from '../../providers/practices';
+import { Globals } from '../../shared/globals';
 
 @Component({
   selector: 'page-login',
@@ -14,7 +16,7 @@ export class LoginPage {
     password: ''
   };
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private _practice: PracticesProvider, public globals: Globals) {
   }
 
   ionViewDidLoad() {
@@ -26,6 +28,17 @@ export class LoginPage {
   }
 
   doLogin(form: NgForm): void {
+    // load list 
+    this._practice.getAllPratices().then((res: any) => {
+      console.log("Success in getting all practices list");
+      this.globals.praticaList = res;
+      // hide loading spinner
+    })
+    .catch(err => {
+      console.log("failed in getting all practices list");
+      console.log(err);
+      // hide loading spinner
+    });
     this.navCtrl.setRoot(CorePage);
   }
 
