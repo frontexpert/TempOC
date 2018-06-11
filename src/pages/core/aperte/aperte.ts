5a0b2589e8c34d5dbb9ce4adf12de06e147356ed
frontexpert <frontexport1987@gmail.com>
2018-06-11 09:31:24 +0900

    Pre-loading for get city and country

--- src/pages/core/aperte/aperte.ts
+++ src/pages/core/aperte/aperte.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { TargaPage } from './targa/targa';
 import { Globals } from '../../../shared/globals';
+import { GeneralProvider } from '../../../providers/general';
 
 import * as Constants from '../../../shared/constants';
 
@@ -15,17 +16,29 @@ export class ApertePage {
 
   CONSTANTS = Constants;
 
-  constructor(public navCtrl: NavController, public globalse: Globals) {    
+  constructor(public navCtrl: NavController, private general: GeneralProvider, public globals: Globals) {    
   }
 
   ionViewDidLoad() {
+    this.fetchCoutryAndCityInBackground();
   }
 
   nextPage(tipoID: number) {    
     console.log('tipoID', tipoID);
     // set tipoID to global service
-    this.globalse.praticaTipoID = tipoID;
+    this.globals.praticaTipoID = tipoID;
     this.navCtrl.push(TargaPage);
+  }
+
+  /**
+   * Get the Country and City from api
+   */
+  private fetchCoutryAndCityInBackground(): void {
+    Promise.all([this.general.getCountry(), this.general.getComune()])
+      .then((values: any[]) => {
+        // nothing to do
+      })
+      .catch(err => console.log('ERROR: ', err));
   }
 
 }
